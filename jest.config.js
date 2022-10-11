f8f1de1b22776e830a6ffeada38b498e911cdae5
二货爱吃白萝卜 <smith3816@gmail.com>
2022-10-11 11:39:06 +0800

    Revert "fix: Fix blink on the first render (#21)" (#22)
    This reverts commit 6e2b4aabf056297bde9f77d6cdb260e9c9b46fac.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  setupFilesAfterEnv: ['./tests/setupFilesAfterEnv.ts'],
   snapshotSerializers: [require.resolve('enzyme-to-json/serializer')],
 };
--- src/ResizableTextArea.tsx
+++ src/ResizableTextArea.tsx
@@ -1,202 +1,177 @@
 import * as React from 'react';
 import ResizeObserver from 'rc-resize-observer';
-import useLayoutEffect from 'rc-util/lib/hooks/useLayoutEffect';
-import raf from 'rc-util/lib/raf';
-import useMergedState from 'rc-util/lib/hooks/useMergedState';
+import omit from 'rc-util/lib/omit';
 import classNames from 'classnames';
-import calculateAutoSizeStyle from './calculateNodeHeight';
+import calculateNodeHeight from './calculateNodeHeight';
 import type { TextAreaProps } from '.';
+import shallowEqual from 'shallowequal';
 
-const RESIZE_START = 0;
-const RESIZE_MEASURING = 1;
-const RESIZE_STABLE = 2;
+// eslint-disable-next-line @typescript-eslint/naming-convention
+enum RESIZE_STATUS {
+  NONE,
+  RESIZING,
+  RESIZED,
+}
 
 export interface AutoSizeType {
   minRows?: number;
   maxRows?: number;
 }
 
-// To compatible with origin usage. We have to wrap this
-export interface ResizableTextAreaRef {
-  textArea: HTMLTextAreaElement;
+export interface TextAreaState {
+  textareaStyles?: React.CSSProperties;
+  /** We need add process style to disable scroll first and then add back to avoid unexpected scrollbar  */
+  resizeStatus?: RESIZE_STATUS;
 }
 
-const ResizableTextArea = React.forwardRef<ResizableTextAreaRef, TextAreaProps>(
-  (props, ref) => {
+class ResizableTextArea extends React.Component<TextAreaProps, TextAreaState> {
+  nextFrameActionId!: number;
+
+  resizeFrameId!: number;
+
+  constructor(props: TextAreaProps) {
+    super(props);
+    this.state = {
+      textareaStyles: {},
+      resizeStatus: RESIZE_STATUS.NONE,
+    };
+  }
+
+  textArea!: HTMLTextAreaElement;
+
+  saveTextArea = (textArea: HTMLTextAreaElement) => {
+    this.textArea = textArea;
+  };
+
+  componentDidUpdate(prevProps: TextAreaProps) {
+    // Re-render with the new content or new autoSize property then recalculate the height as required.
+    if (
+      prevProps.value !== this.props.value ||
+      !shallowEqual(prevProps.autoSize, this.props.autoSize)
+    ) {
+      this.resizeTextarea();
+    }
+  }
+
+  handleResize = (size: { width: number; height: number }) => {
+    const { resizeStatus } = this.state;
+    const { autoSize, onResize } = this.props;
+    if (resizeStatus !== RESIZE_STATUS.NONE) {
+      return;
+    }
+
+    if (typeof onResize === 'function') {
+      onResize(size);
+    }
+    if (autoSize) {
+      this.resizeOnNextFrame();
+    }
+  };
+
+  resizeOnNextFrame = () => {
+    cancelAnimationFrame(this.nextFrameActionId);
+    this.nextFrameActionId = requestAnimationFrame(this.resizeTextarea);
+  };
+
+  resizeTextarea = () => {
+    const { autoSize } = this.props;
+    if (!autoSize || !this.textArea) {
+      return;
+    }
+    const { minRows, maxRows } = autoSize as AutoSizeType;
+    const textareaStyles = calculateNodeHeight(
+      this.textArea,
+      false,
+      minRows,
+      maxRows,
+    );
+    this.setState(
+      { textareaStyles, resizeStatus: RESIZE_STATUS.RESIZING },
+      () => {
+        cancelAnimationFrame(this.resizeFrameId);
+        this.resizeFrameId = requestAnimationFrame(() => {
+          this.setState({ resizeStatus: RESIZE_STATUS.RESIZED }, () => {
+            this.resizeFrameId = requestAnimationFrame(() => {
+              this.setState({ resizeStatus: RESIZE_STATUS.NONE });
+              this.fixFirefoxAutoScroll();
+            });
+          });
+        });
+      },
+    );
+  };
+
+  componentWillUnmount() {
+    cancelAnimationFrame(this.nextFrameActionId);
+    cancelAnimationFrame(this.resizeFrameId);
+  }
+
+  // https://github.com/ant-design/ant-design/issues/21870
+  fixFirefoxAutoScroll() {
+    try {
+      if (document.activeElement === this.textArea) {
+        const currentStart = this.textArea.selectionStart;
+        const currentEnd = this.textArea.selectionEnd;
+        this.textArea.setSelectionRange(currentStart, currentEnd);
+      }
+    } catch (e) {
+      // Fix error in Chrome:
+      // Failed to read the 'selectionStart' property from 'HTMLInputElement'
+      // http://stackoverflow.com/q/21177489/3040605
+    }
+  }
+
+  renderTextArea = () => {
     const {
       prefixCls = 'rc-textarea',
-      onPressEnter,
-      defaultValue,
-      value,
       autoSize,
       onResize,
       className,
-      style,
       disabled,
-      onChange,
-      // Test only
-      onInternalAutoSize,
-      ...restProps
-    } = props as TextAreaProps & {
-      onInternalAutoSize?: VoidFunction;
-    };
-
-    // =============================== Value ================================
-    const [mergedValue, setMergedValue] = useMergedState(defaultValue, {
-      value,
-      postState: (val) => val ?? '',
+    } = this.props;
+    const { textareaStyles, resizeStatus } = this.state;
+    const otherProps: any = omit(this.props, [
+      'prefixCls',
+      'onPressEnter',
+      'autoSize',
+      'defaultValue',
+      'onResize',
+    ]);
+    const cls = classNames(prefixCls, className, {
+      [`${prefixCls}-disabled`]: disabled,
     });
-
-    const onInternalChange: React.ChangeEventHandler<HTMLTextAreaElement> = (
-      event,
-    ) => {
-      setMergedValue(event.target.value);
-      onChange?.(event);
-    };
-
-    // ================================ Ref =================================
-    const textareaRef = React.useRef<HTMLTextAreaElement>();
-
-    React.useImperativeHandle(ref, () => ({
-      textArea: textareaRef.current,
-    }));
-
-    // ============================== AutoSize ==============================
-    const [minRows, maxRows] = React.useMemo(() => {
-      if (autoSize && typeof autoSize === 'object') {
-        return [autoSize.minRows, autoSize.maxRows];
-      }
-
-      return [];
-    }, [autoSize]);
-
-    const needAutoSize = !!autoSize;
-
-    // =============================== Scroll ===============================
-    // https://github.com/ant-design/ant-design/issues/21870
-    const fixFirefoxAutoScroll = () => {
-      try {
-        // FF has bug with jump of scroll to top. We force back here.
-        if (document.activeElement === textareaRef.current) {
-          const { selectionStart, selectionEnd, scrollTop } =
-            textareaRef.current;
-
-          const { value: tmpValue } = textareaRef.current;
-          textareaRef.current.value = '';
-          textareaRef.current.value = tmpValue;
-
-          textareaRef.current.setSelectionRange(selectionStart, selectionEnd);
-          textareaRef.current.scrollTop = scrollTop;
-        }
-      } catch (e) {
-        // Fix error in Chrome:
-        // Failed to read the 'selectionStart' property from 'HTMLInputElement'
-        // http://stackoverflow.com/q/21177489/3040605
-      }
-    };
-
-    // =============================== Resize ===============================
-    const [resizeState, setResizeState] = React.useState(RESIZE_STABLE);
-    const [autoSizeStyle, setAutoSizeStyle] =
-      React.useState<React.CSSProperties>();
-
-    const startResize = () => {
-      setResizeState(RESIZE_START);
-      if (process.env.NODE_ENV === 'test') {
-        onInternalAutoSize?.();
-      }
-    };
-
-    // Change to trigger resize measure
-    useLayoutEffect(() => {
-      if (needAutoSize) {
-        startResize();
-      }
-    }, [value, minRows, maxRows, needAutoSize]);
-
-    useLayoutEffect(() => {
-      if (resizeState === RESIZE_START) {
-        setResizeState(RESIZE_MEASURING);
-      } else if (resizeState === RESIZE_MEASURING) {
-        const textareaStyles = calculateAutoSizeStyle(
-          textareaRef.current,
-          false,
-          minRows,
-          maxRows,
-        );
-
-        // Safari has bug that text will keep break line on text cut when it's prev is break line.
-        // ZombieJ: This not often happen. So we just skip it.
-        // const { selectionStart, selectionEnd, scrollTop } = textareaRef.current;
-        // const { value: tmpValue } = textareaRef.current;
-        // textareaRef.current.value = '';
-        // textareaRef.current.value = tmpValue;
-
-        // if (document.activeElement === textareaRef.current) {
-        //   textareaRef.current.scrollTop = scrollTop;
-        //   textareaRef.current.setSelectionRange(selectionStart, selectionEnd);
-        // }
-
-        setResizeState(RESIZE_STABLE);
-        setAutoSizeStyle(textareaStyles);
-      } else {
-        fixFirefoxAutoScroll();
-      }
-    }, [resizeState]);
-
-    // We lock resize trigger by raf to avoid Safari warning
-    const resizeRafRef = React.useRef<number>();
-    const cleanRaf = () => {
-      raf.cancel(resizeRafRef.current);
-    };
-
-    const onInternalResize = (size: { width: number; height: number }) => {
-      if (resizeState === RESIZE_STABLE) {
-        onResize?.(size);
-
-        if (autoSize) {
-          cleanRaf();
-          resizeRafRef.current = raf(() => {
-            startResize();
-          });
-        }
-      }
-    };
-
-    React.useEffect(() => cleanRaf, []);
-
-    // =============================== Render ===============================
-    const mergedAutoSizeStyle = needAutoSize ? autoSizeStyle : null;
-
-    const mergedStyle = {
-      ...style,
-      ...mergedAutoSizeStyle,
-    };
-
-    if (resizeState === RESIZE_START || resizeState === RESIZE_MEASURING) {
-      mergedStyle.overflowY = 'hidden';
-      mergedStyle.overflowX = 'hidden';
+    // Fix https://github.com/ant-design/ant-design/issues/6776
+    // Make sure it could be reset when using form.getFieldDecorator
+    if ('value' in otherProps) {
+      otherProps.value = otherProps.value || '';
     }
-
+    const style: React.CSSProperties = {
+      ...this.props.style,
+      ...textareaStyles,
+      ...(resizeStatus === RESIZE_STATUS.RESIZING
+        ? // React will warning when mix `overflow` & `overflowY`.
+          // We need to define this separately.
+          { overflowX: 'hidden', overflowY: 'hidden' }
+        : null),
+    };
     return (
       <ResizeObserver
-        onResize={onInternalResize}
+        onResize={this.handleResize}
         disabled={!(autoSize || onResize)}
       >
         <textarea
-          {...restProps}
-          ref={textareaRef}
-          style={mergedStyle}
-          className={classNames(prefixCls, className, {
-            [`${prefixCls}-disabled`]: disabled,
-          })}
-          disabled={disabled}
-          value={mergedValue}
-          onChange={onInternalChange}
+          {...otherProps}
+          className={cls}
+          style={style}
+          ref={this.saveTextArea}
         />
       </ResizeObserver>
     );
-  },
-);
+  };
+
+  render() {
+    return this.renderTextArea();
+  }
+}
 
 export default ResizableTextArea;
